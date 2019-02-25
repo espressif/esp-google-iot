f06efc3353fea76fe09d4e247884b9a74703983c
Mahavir Jain <mahavir@espressif.com>
2019-02-25 19:00:08 +0530

    Fix cmake build and also code formatting

--- component.mk
+++ component.mk
@@ -3,57 +3,57 @@
 #
 
 COMPONENT_ADD_INCLUDEDIRS := \
-        iot-edge-sdk-embedded-c/src/bsp \
-        iot-edge-sdk-embedded-c/include/bsp \
-        iot-edge-sdk-embedded-c/include \
-        iot-edge-sdk-embedded-c/src/libiotc/control_topic \
-        iot-edge-sdk-embedded-c/src/libiotc/datastructures \
-        iot-edge-sdk-embedded-c/src/libiotc/debug_extensions/memory_limiter \
-        iot-edge-sdk-embedded-c/src/libiotc/event_dispatcher \
-        iot-edge-sdk-embedded-c/src/libiotc/event_loop \
-        iot-edge-sdk-embedded-c/src/libiotc/io/fs/memory \
-        iot-edge-sdk-embedded-c/src/libiotc/io/fs \
-        iot-edge-sdk-embedded-c/src/libiotc/io/net \
-        iot-edge-sdk-embedded-c/src/libiotc/mqtt/logic \
-        iot-edge-sdk-embedded-c/src/libiotc/mqtt/codec \
-        iot-edge-sdk-embedded-c/src/libiotc/platform/iotc_thread \
-        iot-edge-sdk-embedded-c/src/libiotc/platform/posix/iotc_thread \
-        iot-edge-sdk-embedded-c/src/libiotc/tls \
-        iot-edge-sdk-embedded-c/src/libiotc/tls/certs \
-        iot-edge-sdk-embedded-c/src/libiotc/memory \
-        iot-edge-sdk-embedded-c/src/libiotc \
-        iot-edge-sdk-embedded-c/third_party/mqtt-protocol-c \
+        iot-device-sdk-embedded-c/src/bsp \
+        iot-device-sdk-embedded-c/include/bsp \
+        iot-device-sdk-embedded-c/include \
+        iot-device-sdk-embedded-c/src/libiotc/control_topic \
+        iot-device-sdk-embedded-c/src/libiotc/datastructures \
+        iot-device-sdk-embedded-c/src/libiotc/debug_extensions/memory_limiter \
+        iot-device-sdk-embedded-c/src/libiotc/event_dispatcher \
+        iot-device-sdk-embedded-c/src/libiotc/event_loop \
+        iot-device-sdk-embedded-c/src/libiotc/io/fs/memory \
+        iot-device-sdk-embedded-c/src/libiotc/io/fs \
+        iot-device-sdk-embedded-c/src/libiotc/io/net \
+        iot-device-sdk-embedded-c/src/libiotc/mqtt/logic \
+        iot-device-sdk-embedded-c/src/libiotc/mqtt/codec \
+        iot-device-sdk-embedded-c/src/libiotc/platform/iotc_thread \
+        iot-device-sdk-embedded-c/src/libiotc/platform/posix/iotc_thread \
+        iot-device-sdk-embedded-c/src/libiotc/tls \
+        iot-device-sdk-embedded-c/src/libiotc/tls/certs \
+        iot-device-sdk-embedded-c/src/libiotc/memory \
+        iot-device-sdk-embedded-c/src/libiotc \
+        iot-device-sdk-embedded-c/third_party/mqtt-protocol-c \
         port/include
 
 
 
 COMPONENT_SRCDIRS := \
-        iot-edge-sdk-embedded-c/src/bsp/tls/mbedtls \
-        iot-edge-sdk-embedded-c/src/bsp/crypto/mbedtls \
-        iot-edge-sdk-embedded-c/src/libiotc \
-        iot-edge-sdk-embedded-c/src/libiotc/control_topic \
-        iot-edge-sdk-embedded-c/src/libiotc/datastructures \
-        iot-edge-sdk-embedded-c/src/libiotc/debug_extensions/memory_limiter \
-        iot-edge-sdk-embedded-c/src/libiotc/event_dispatcher \
-        iot-edge-sdk-embedded-c/src/libiotc/event_loop \
-        iot-edge-sdk-embedded-c/src/libiotc/io/fs \
-        iot-edge-sdk-embedded-c/src/libiotc/io/fs/memory \
-        iot-edge-sdk-embedded-c/src/libiotc/io/net \
-        iot-edge-sdk-embedded-c/src/libiotc/memory \
-        iot-edge-sdk-embedded-c/src/libiotc/mqtt/codec \
-        iot-edge-sdk-embedded-c/src/libiotc/mqtt/logic \
-        iot-edge-sdk-embedded-c/src/libiotc/mqtt/tls \
-        iot-edge-sdk-embedded-c/src/libiotc/tls \
-        iot-edge-sdk-embedded-c/src/libiotc/tls/certs \
-        iot-edge-sdk-embedded-c/third_party/mqtt-protocol-c \
+        iot-device-sdk-embedded-c/src/bsp/tls/mbedtls \
+        iot-device-sdk-embedded-c/src/bsp/crypto/mbedtls \
+        iot-device-sdk-embedded-c/src/libiotc \
+        iot-device-sdk-embedded-c/src/libiotc/control_topic \
+        iot-device-sdk-embedded-c/src/libiotc/datastructures \
+        iot-device-sdk-embedded-c/src/libiotc/debug_extensions/memory_limiter \
+        iot-device-sdk-embedded-c/src/libiotc/event_dispatcher \
+        iot-device-sdk-embedded-c/src/libiotc/event_loop \
+        iot-device-sdk-embedded-c/src/libiotc/io/fs \
+        iot-device-sdk-embedded-c/src/libiotc/io/fs/memory \
+        iot-device-sdk-embedded-c/src/libiotc/io/net \
+        iot-device-sdk-embedded-c/src/libiotc/memory \
+        iot-device-sdk-embedded-c/src/libiotc/mqtt/codec \
+        iot-device-sdk-embedded-c/src/libiotc/mqtt/logic \
+        iot-device-sdk-embedded-c/src/libiotc/mqtt/tls \
+        iot-device-sdk-embedded-c/src/libiotc/tls \
+        iot-device-sdk-embedded-c/src/libiotc/tls/certs \
+        iot-device-sdk-embedded-c/third_party/mqtt-protocol-c \
         port/src
 
-COMPONENT_SUBMODULES := iot-edge-sdk-embedded-c
+COMPONENT_SUBMODULES := iot-device-sdk-embedded-c
 
-COMPONENT_OBJEXCLUDE := iot-edge-sdk-embedded-c/src/libiotc/iotc_test.o \
-                        iot-edge-sdk-embedded-c/src/libiotc/iotc_jwt_test.o \
-                        iot-edge-sdk-embedded-c/src/libiotc/event_dispatcher/iotc_event_dispatcher_test.o \
-                        iot-edge-sdk-embedded-c/src/libiotc/event_dispatcher/iotc_event_handle_test.o
+COMPONENT_OBJEXCLUDE := iot-device-sdk-embedded-c/src/libiotc/iotc_test.o \
+                        iot-device-sdk-embedded-c/src/libiotc/iotc_jwt_test.o \
+                        iot-device-sdk-embedded-c/src/libiotc/event_dispatcher/iotc_event_dispatcher_test.o \
+                        iot-device-sdk-embedded-c/src/libiotc/event_dispatcher/iotc_event_handle_test.o
 
 CFLAGS += -DIOTC_FS_MEMORY \
           -DIOTC_MEMORY_LIMITER_APPLICATION_MEMORY_LIMIT=524288 \
